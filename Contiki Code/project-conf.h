3e9e5af7a7f3036bfd11e438f7ccb9071d5658c5
George Smart <george.smart1@btinternet.com>
2016-01-11 23:09:24 +0000

    Uploaded Contiki Code & Readme

--- Contiki Code/project-conf.h
+++ Contiki Code/project-conf.h
@@ -1,5 +1,6 @@
-// George Smart, Thu 9 Aug 2012.
-// http://www.sics.se/contiki/wiki/index.php/Change_MAC_or_Radio_Duty_Cycling_Protocols
+// George Smart, Jan 2016
+// https://github.com/contiki-os/contiki/wiki/Change-mac-or-radio-duty-cycling-protocols
+// http://www.sics.se/contiki/wiki/index.php/Change_MAC_or_Radio_Duty_Cycling_Protocols (old)
 
 // Change the RDC and MAC to null_drivers.
 #define NETSTACK_CONF_RDC nullrdc_driver
